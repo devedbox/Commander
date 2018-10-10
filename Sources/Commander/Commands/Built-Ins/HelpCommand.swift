4a502e127ff2d87bf3e09c96c692d65c2cadc5ed
devedbox <862099730@qq.com>
2018-10-10 23:30:09 +0800

    feat: Adds describing of commander and commands.

--- Sources/Commander/Commands/Built-Ins/HelpCommand.swift
+++ Sources/Commander/Commands/Built-Ins/HelpCommand.swift
@@ -34,6 +34,25 @@ extension FileHandle: TextOutputStream {
   }
 }
 
+// MARK: - AnyCommandRepresentable.
+
+extension AnyCommandRepresentable {
+  internal static func evaluate(_ symbols: [String]) -> (command: AnyCommandRepresentable.Type?, symbol: [String]) {
+    if symbol == symbols.first {
+      let subsymbols = Array(symbols.dropFirst())
+      if
+        !subcommands.isEmpty,
+        let subcommand = subcommands.first(where: { $0.symbol == subsymbols.first })
+      {
+        return subcommand.evaluate(subsymbols)
+      } else {
+        return (self, Array(symbols.dropFirst()))
+      }
+    }
+    return (nil, symbols)
+  }
+}
+
 // MARK: - HelpCommand.
 
 /// The built-in help command for the commander.
@@ -97,35 +116,12 @@ internal struct HelpCommand: CommandRepresentable {
   internal static func main(_ options: Options) throws {
     var stdout = FileHandle.standardOutput
     
-    func intents(_ level: Int) -> String {
-      return String(repeating: " ", count: 2 * (level + (options.intents ?? 0)))
-    }
-    
-    func returns(_ level: Int) -> String {
-      return String(repeating: "\n", count: max(0, (level - (options.intents ?? 0))))
-    }
-    
-    let path = Commander.runningPath.split(separator: "/").last!
-    
     if options.arguments.isEmpty {
-      let prefix = """
-      Usage:
-      \(returns(0))
-      \(intents(1))$ \(path) COMMAND
-      \(returns(0))
-      \(intents(2))\(Commander.usage)
-      \(returns(0))
-      Commands:
-      
-      """
-      let sample = String(repeating: " ", count: Commander.allCommands.reduce(0) { max($0, $1.symbol.count) })
-      let commands = Commander.allCommands.map { command -> String in
-        var fixedSymbol = sample
-        fixedSymbol.replaceSubrange(command.symbol.startIndex..<command.symbol.endIndex, with: command.symbol)
-        return fixedSymbol + intents(1) + command.usage
-        }.joined(separator: "\n\(intents(1))")
-      
-      print(prefix, commands, separator: "\n  ", terminator: "\n", to: &stdout)
+      print(
+        CommandDescriber().describe(Commander.self),
+        terminator: "\n",
+        to: &stdout
+      )
       
       /* FIXME: Disable the subcommands' description for no prefered formats for now.
        print(prefix, commands, "\nDescriptions:", separator: "\n  ", terminator: "\n\n", to: &stdout)
@@ -148,63 +144,11 @@ internal struct HelpCommand: CommandRepresentable {
         throw CommanderError.helpUnrecognizedCommands(commands: unrecognizedCommand)
       }
       
-      let commandSymbols = options.arguments
-      
-      var sample = String(repeating: " ", count: path.count + 1 + commandSymbols.reduce(0) { max($0, $1.count) })
-      let commandsOutputs = commands.map { cmd -> String in
-        let optionsOutput = cmd.optionsDescriber.descriptions.isEmpty ? "" : " [OPTIONS]"
-        let argumentsOutput = cmd.optionsDescriber.isArgumentsResolvable ? " [ARGUMENTS]" : ""
-        var symbol = sample
-        let contents = "\(path) \(cmd.symbol)"
-        symbol.replaceSubrange(contents.startIndex..<contents.endIndex, with: contents)
-        return """
-        \(intents(0))Usage of '\(cmd.symbol)':
-        \(returns(0))
-        \(intents(1))$ \(symbol)\(optionsOutput)\(argumentsOutput)\(returns(1))
-        \(intents(2))\(cmd.usage)
-        \(returns(0))
-        \(intents(1))Options:
-        \(returns(0))
-        """
-      }
-      
-      sample = String(repeating: " ", count: commands.map {
-        ("[\(String(describing: $0.optionsDescriber.argumentType))]", ($0.optionsDescriber.descriptions, $0.optionsDescriber.keys))
-        }.reduce(0) {
-          let keys = $1.1.1
-          return max(max($0, $1.0.count), $1.1.0.reduce(0) {
-            max($0, ((keys[$1.key].map { "-\($0), "  } ?? "") + "--\($1.key)").count)
-          })
-      })
-      
-      let outputs = commands.enumerated().map { index, command -> String in
-        let prefix = commandsOutputs[index]
-        let keys = command.optionsDescriber.keys
-        let options = command.optionsDescriber.descriptions.map { desc -> String in
-          var fixedSymbol = sample
-          let symbol = (keys[desc.key].map { "-\($0), " } ?? "") + "--\(desc.key)"
-          fixedSymbol.replaceSubrange(symbol.startIndex..<symbol.endIndex, with: symbol)
-          return intents(2) + fixedSymbol + intents(1) + desc.1.usage
-          }.joined(separator: "\n")
-        
-        var suffix = ""
-        if command.optionsDescriber.isArgumentsResolvable {
-          let argType = "[\(String(describing: command.optionsDescriber.argumentType))]"
-          var symbol = sample
-          symbol.replaceSubrange(argType.startIndex..<argType.endIndex, with: argType)
-          suffix = """
-          \(returns(0))
-          \(intents(1))Arguments:
-          \(returns(0))
-          \(intents(2))\(symbol)\(intents(1))\(path) \(command.symbol) [options] arg1 arg2 ...
-          
-          """
-        }
-        
-        return prefix + "\n" + options + "\n" + suffix
-        }.joined(separator: "\n")
-      
-      print(outputs, terminator: "", to: &stdout)
+      print(
+        commands.map { CommandDescriber().describe($0) }.joined(separator: "\n\n"),
+        terminator: "\n",
+        to: &stdout
+      )
     }
   }
 }
--- Sources/Commander/Commands/CommandDescriber.swift
+++ Sources/Commander/Commands/CommandDescriber.swift
@@ -25,20 +25,119 @@
 
 import Foundation
 
+extension String {
+  internal func merging(_ other: String) -> String {
+    var merging: String
+    let merged: String
+    
+    if self.endIndex > other.endIndex {
+      merging = self; merged = other
+    } else {
+      merging = other; merged = self
+    }
+    
+    merging.replaceSubrange(merged.startIndex..<merged.endIndex, with: merged)
+    return merging
+  }
+}
+
 internal struct CommandDescriber {
-  internal func describe(_ command: AnyCommandRepresentable.Type) throws -> String {
-    return ""
+  private let intents: Int
+  
+  internal init(intents: Int = 0) {
+    self.intents = intents
+  }
+  
+  internal func describe(_ commander: Commander.Type) -> String {
+    let path = Commander.runningPath.split(separator: "/").last!
+    
+    let commandsSymbols = commander.allCommands.map { ($0.symbol, $0.usage) }
+    
+    let count = commandsSymbols.reduce(0) { max($0, $1.0.count) }
+    let alignment = String(repeating: " ", count: count)
+    
+    return """
+    Usage:
+    \(returns(0))
+    \(intents(1))$ \(path) COMMAND
+    \(returns(0))
+    \(intents(2))\(Commander.usage)
+    \(returns(0))
+    Commands:
+    \(returns(0))
+    \(commandsSymbols.map { intents(2) + $0.0.merging(alignment) + intents(1) + $0.1 }.joined(separator: "\n"))
+    """
+  }
+  
+  internal func describe(_ command: AnyCommandRepresentable.Type) -> String {
+    // var sample = String(repeating: " ", count: path.count + 1 + commandSymbols.reduce(0) { max($0, $1.count) })
+    let path = String(Commander.runningPath.split(separator: "/").last!)
+    
+    let subcommandSymbols = command.subcommands.map { ($0.symbol, $0.usage) }
+    let optionsSymbols = command.optionsDescriber.descriptions.map { desc -> (String, String) in
+      let shortKey = command.optionsDescriber.keys[desc.key]
+      let keyDesc = (shortKey.map { "-\($0), " } ?? "") + "--\(desc.key)"
+      return (keyDesc, desc.value.usage)
+    }
+    let argumentsSymbols: [(String, String)] = command.optionsDescriber.isArgumentsResolvable == false ? [] : [
+      ("[\(String(describing: command.optionsDescriber.argumentType))]", "\(path) \(command.symbol) [options] arg1 arg2 ...")
+    ]
+    
+    let count = (subcommandSymbols + optionsSymbols + argumentsSymbols).reduce(0) {
+      return max($0, $1.0.count)
+    }
+    let alignment = String(repeating: " ", count: count)
+    
+    let subcommandsSummary = subcommandSymbols.isEmpty ? "" : " [SUBCOMMANDS]"
+    let optionsSummary = optionsSymbols.isEmpty ? "" : " [OPTIONS]"
+    let argumentsSummary = command.optionsDescriber.isArgumentsResolvable ? " [ARGUMENTS]" : ""
+    
+    let subcommandsLabel = subcommandSymbols.isEmpty ? "" : """
+    \(returns(1))
+    \(intents(1))Subcommands:
+    \(returns(0))
+    """
+    let optionsLabel = optionsSymbols.isEmpty ? "" : """
+    \(returns(1))
+    \(intents(1))Options:
+    \(returns(0))
+    """
+    let argumentsLabel = command.optionsDescriber.isArgumentsResolvable == false ? "" : """
+    \(returns(1))
+    \(intents(1))Arguments:
+    \(returns(0))
+    """
+    
+    let subcommandsOutputs = subcommandsLabel.isEmpty ? "" : """
+    \(subcommandsLabel)
+    \(subcommandSymbols.map { intents(2) + $0.0.merging(alignment) + intents(1) + $0.1 }.joined(separator: "\n"))
+    """
+    
+    let optionsOutputs = optionsLabel.isEmpty ? "" : """
+    \(optionsLabel)
+    \(optionsSymbols.map    { intents(2) + $0.0.merging(alignment) + intents(1) + $0.1 }.joined(separator: "\n"))
+    """
+    
+    let argumentsOutputs = argumentsLabel.isEmpty ? "" : """
+    \(argumentsLabel)
+    \(argumentsSymbols.map  { intents(2) + $0.0.merging(alignment) + intents(1) + $0.1 }.joined(separator: "\n"))
+    """
+    
+    return """
+    \(intents(0))Usage of '\(command.symbol)':
+    \(returns(0))
+    \(intents(1))$ \(path) \(command.symbol) \(subcommandsSummary)\(optionsSummary)\(argumentsSummary)\(returns(1))
+    \(intents(2))\(command.usage)\(subcommandsOutputs)\(optionsOutputs)\(argumentsOutputs)
+    """
   }
 }
 
 extension CommandDescriber {
-  private func intents(_ level: Int, basis: Int? = 0) -> String {
-    // return String(repeating: " ", count: 2 * (level + (options.intents ?? 0)))
-    return String(repeating: " ", count: 2 * (level + (basis ?? 0)))
+  private func intents(_ level: Int) -> String {
+    return String(repeating: " ", count: 2 * (level + intents))
   }
   
   private func returns(_ level: Int, basis: Int? = 0) -> String {
-    // return String(repeating: "\n", count: max(0, (level - (options.intents ?? 0))))
-    return String(repeating: "\n", count: max(0, (level - (basis ?? 0))))
+    return String(repeating: "\n", count: max(0, (level - intents)))
   }
 }
