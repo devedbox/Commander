1720e1d621d2a8a62636f7820a81465bd2c9c0e5
devedbox <devedbox@qq.com>
2018-11-02 16:39:27 +0800

    feat: Adds command complete to generate completion script

---
+++ Sources/Commander/Commands/Built-Ins/Complete.swift
@@ -0,0 +1,51 @@
+//
+//  Complete.swift
+//  Commander
+//
+//  Created by devedbox on 2018/11/2.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+
+/// The command to add bash-completion scripts of the commander
+internal struct Complete: CommandRepresentable {
+  internal struct Options: OptionsRepresentable {
+    internal enum ShellMode: String, Codable {
+      case bash
+    }
+    
+    internal enum CodingKeys: String, CodingKeysRepresentable {
+      case mode
+    }
+    
+    internal static var keys: [CodingKeys: Character] = [:]
+    internal static var descriptions: [CodingKeys: OptionDescription] = [:]
+    
+    internal let mode: ShellMode
+  }
+  
+  internal static let symbol = "complete"
+  internal static let usage = "Auto add the shell completion script of the commander"
+  
+  internal static func main(_ options: Complete.Options) throws {
+    
+  }
+}
