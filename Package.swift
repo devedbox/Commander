06f441c48889f2c7cea7b2a0e670e62a6cc03317
devedbox <862099730@qq.com>
2018-11-09 22:47:42 +0800

    chore: Adds target 'Utility'

--- Package.swift
+++ Package.swift
@@ -21,6 +21,12 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(
       name: "Commander",
+      dependencies: [
+        "Utility"
+      ]
+    ),
+    .target(
+      name: "Utility",
       dependencies: []
     ),
     .target(
--- Sources/Commander/Built-Ins/Commands/Complete.swift
+++ Sources/Commander/Built-Ins/Commands/Complete.swift
@@ -24,6 +24,7 @@
 //
 
 import Foundation
+import Utility
 
 // MARK: - Complete.Generate
 
