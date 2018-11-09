3ce445ef66b6de2a6436d6295a098a2d67be4203
devedbox <862099730@qq.com>
2018-11-09 23:04:10 +0800

    feat: Adds type 'CommandLine'

---
+++ Sources/Utility/CommandLine.swift
@@ -0,0 +1,47 @@
+//
+//  CommandLine.swift
+//  Utility
+//
+//  Created by devedbox on 2018/11/9.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+
+/// A type that parses the command line arguments.
+public struct CommandLine {
+  /// Indicates the reading of command line is quoting.
+  internal private(set) var isQuoting: Bool = false
+  /// Indicates the reading of command line is escaping.
+  internal private(set) var isEscaping: Bool = false
+  /// The parsed arguments.
+  public private(set) var arguments: [String] = []
+  
+  /// Parse the given command line string.
+  public mutating func parse(_ commandLine: String) throws {
+    for char in commandLine {
+      switch char {
+      case "\\": isEscaping = true
+      case "\"": isQuoting = true
+      default:
+      }
+    }
+  }
+}
